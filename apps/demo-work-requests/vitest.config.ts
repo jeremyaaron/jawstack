890eb0dfd5b581133c22a622a69e5c8fa8f89408
Jeremy Aaron <shyapologies@gmail.com>
2026-06-26 22:57:46 -0400

    feat: update vitest configuration to include alias resolution for packages

--- apps/demo-work-requests/vitest.config.ts
+++ apps/demo-work-requests/vitest.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@jawstack/angular": new URL("../../packages/angular/src/index.ts", import.meta.url).pathname,
+      "@jawstack/aws-cdk": new URL("../../packages/aws-cdk/src/index.ts", import.meta.url).pathname,
+      "@jawstack/aws-runtime": new URL("../../packages/aws-runtime/src/index.ts", import.meta.url)
+        .pathname,
+      "@jawstack/cli": new URL("../../packages/cli/src/index.ts", import.meta.url).pathname,
+      "@jawstack/core": new URL("../../packages/core/src/index.ts", import.meta.url).pathname,
+    },
+  },
   test: {
     environment: "node",
   },
